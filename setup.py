10aba8006404c16b980c45ae09c4ed797992e6cb
Jim Steil <jim@steilonline.com>
2022-07-20 13:32:58 -0500

    updated setup.py

--- setup.py
+++ setup.py
@@ -1,20 +1,3 @@
-from setuptools import setup
-
-setup(
-    name="pydlfmt",
-    version="0.1",
-    description="Datalist Formatter",
-    url="https://pydlfmt.com",
-    author="Jim Steil",
-    author_email="jim@qlf.com",
-    license="MIT",
-    packages=["pydlfmt"],
-    include_package_data=True,
-    install_requires=["reportlab", "xlsxwriter"],
-    zip_safe=False,
-)
-
-
 from distutils.core import setup
 
 setup(
@@ -24,8 +7,9 @@ setup(
     description="pydlfmt will take a list of objects, dicts, etc and output them to a PDF or XLSX",
     author="Jim Steil",
     author_email="jim@steilonline.com",
-    url="github package source url",
-    download_url="download link you saved",
+    url="https://github.com/jpsteil/pydlfmt/tree/v0.0.1",
+    download_url="https://github.com/jpsteil/pydlfmt/archive/refs/tags/v0.0.1.tar.gz",
     keywords=["tag1", "tag2"],
     classifiers=[],
+    install_requires=["reportlab", "xlsxwriter"],
 )
