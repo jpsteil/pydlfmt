d21009e57478fc509934cc02ed2aae306deecbe6
Jim Steil <jim@steilonline.com>
2022-09-19 09:59:50 -0500

    get version # from github versions

--- setup.py
+++ setup.py
@@ -1,15 +1,27 @@
 from setuptools import setup
 
+pydlfmt_version = subprocess.run(['git', 'describe', '--tags'], stdout=subprocess.PIPE).stdout.decode("utf-8").strip()
+assert "." in pydlfmt_version
+
+assert os.path.isfile("pydlfmt/version.py")
+with open("pydlfmt/VERSION", "w", encoding="utf-8") as fh:
+    fh.write(f"{pydlfmt_version}\n")
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
 setup(
     name="pydlfmt",
     packages=["pydlfmt"],
-    version="v0.0.3",  # Ideally should be same as your GitHub release tag varsion
+    version=pydlfmt_version,  # Ideally should be same as your GitHub release tag varsion
     description="pydlfmt will take a list of objects, dicts, etc and output them to a PDF or XLSX",
     author="Jim Steil",
     author_email="jim@steilonline.com",
-    url="https://github.com/jpsteil/pydlfmt/tree/v0.0.1",
-    download_url="https://github.com/jpsteil/pydlfmt/archive/refs/tags/v0.0.1.tar.gz",
-    keywords=["tag1", "tag2"],
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/jpsteil/pydlfmt",
+#     download_url="https://github.com/jpsteil/pydlfmt/archive/refs/tags/v0.0.1.tar.gz",
+    keywords=[],
     classifiers=[],
     install_requires=["pytest", "reportlab", "xlsxwriter"],
     zip_safe=False,
