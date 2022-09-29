ca28198fcdcf93c9badac1b80a41689ca45607d4
Jim Steil <jim@steilonline.com>
2022-09-29 11:19:32 -0500

    bump version

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ setup(
     name="pydlfmt",
     # package_dir={"": ""},
     packages=["pydlfmt"],
-    version="v0.0.4",  # Ideally should be same as your GitHub release tag varsion
+    version="v0.0.5",  # Ideally should be same as your GitHub release tag varsion
     description="pydlfmt will take a list of objects, dicts, etc and output them to a PDF or XLSX",
     author="Jim Steil",
     author_email="jim@steilonline.com",
